7cb5a8a7c014d675d0a931c9477468aeb4fc7ecf
Abdullah Salah <asr12211@outlook.com>
2024-08-29 00:25:13 +0300

    MORE SETUP

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,11 +1,12 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-color-bg">
 	<!-- Primary Navigation Menu -->
 	<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 		<div class="flex justify-between h-16">
 			<!-- Logo -->
 			<div class="shrink-0 flex items-center">
-				<a href="{{ route('dashboard') }}">
-					<x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+				<a href="{{ route('dashboard') }}" class="w-16 border-red-400 flex items-center">
+					<x-application-logo />
+					<span class="font-cairo text-color-title">Blogosphere</span>
 				</a>
 			</div>
 
@@ -13,7 +14,7 @@
 			<div class="flex">
 				<div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
 					<x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-						{{ __('Dashboard') }}
+						Dashboard
 					</x-nav-link>
 				</div>
 			</div>
@@ -22,7 +23,7 @@
 				<x-dropdown align="right" width="48">
 					<x-slot name="trigger">
 						<button
-							class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+							class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
 							<div>{{ Auth::user()->name }}</div>
 
 							<div class="ms-1">
@@ -37,7 +38,7 @@
 
 					<x-slot name="content">
 						<x-dropdown-link :href="route('profile.edit')">
-							{{ __('Profile') }}
+							Profile
 						</x-dropdown-link>
 
 						<!-- Authentication -->
@@ -46,7 +47,7 @@
 
 							<x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-								{{ __('Log Out') }}
+								Log Out
 							</x-dropdown-link>
 						</form>
 					</x-slot>
@@ -56,7 +57,7 @@
 			<!-- Hamburger -->
 			<div class="-me-2 flex items-center sm:hidden">
 				<button @click="open = ! open"
-					class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+					class="inline-flex items-center justify-center p-2 rounded-md text-color-title hover:bg-color-secondary focus:outline-none focus:bg-color-secondary focus:text-color-title transition duration-150 ease-in-out">
 					<svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
 						<path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round"
 							stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -73,20 +74,20 @@
 	<div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
 		<div class="pt-2 pb-3 space-y-1">
 			<x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-				{{ __('Dashboard') }}
+				Dashboard
 			</x-responsive-nav-link>
 		</div>
 
 		<!-- Responsive Settings Options -->
-		<div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+		<div class="pt-4 pb-1 border-t border-gray-200">
 			<div class="px-4">
-				<div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-				<div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+				<div class="font-medium text-base text-color-title">{{ Auth::user()->name }}</div>
+				<div class="font-medium text-sm text-color-title">{{ Auth::user()->email }}</div>
 			</div>
 
 			<div class="mt-3 space-y-1">
 				<x-responsive-nav-link :href="route('profile.edit')">
-					{{ __('Profile') }}
+					Profile
 				</x-responsive-nav-link>
 
 				<!-- Authentication -->
@@ -95,7 +96,7 @@
 
 					<x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
                                             this.closest('form').submit();">
-						{{ __('Log Out') }}
+						Log Out
 					</x-responsive-nav-link>
 				</form>
 			</div>
--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,11 +1,11 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Update Password') }}
+        <h2 class="text-lg font-medium text-gray-900">
+            Update Password
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
+        <p class="mt-1 text-sm text-gray-600">
+            Ensure your account is using a long, random password to stay secure.
         </p>
     </header>
 
@@ -32,7 +32,7 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button>Save</x-primary-button>
 
             @if (session('status') === 'password-updated')
                 <p
@@ -40,8 +40,8 @@
                     x-show="show"
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
+                    class="text-sm text-gray-600"
+                >Saved.</p>
             @endif
         </div>
     </form>
